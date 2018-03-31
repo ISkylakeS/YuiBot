d0fd204ec5e22b1785f0f11e84ca8eb0b1bf12a9
August (Chris) <view92gaming@gmail.com>
2018-03-30 20:04:48 -0700

    reeee....

---
+++ src/main/java/xyz/yuibot/discord/YuiConfig.java
@@ -0,0 +1,19 @@
+package xyz.yuibot.discord;
+
+public class YuiConfig {
+    public static String token = "";
+    public static String patreon = "";
+    public static String oliyBots = "";
+    public static String terminal = "";
+    public static String blspace = "";
+    public static String prefix = "yui!";
+
+    public static String getToken() {
+        return token;
+    }
+
+    public static String getPatreonToken() {
+        return patreon;
+    }
+
+}
